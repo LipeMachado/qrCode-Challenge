7266139d2c2ca607365ad15ffa36231b75e294c1
LipeMachado <silvalipe765@gmail.com>
2022-03-09 00:12:19 -0300

    Update

--- src/Pages/InitialPage/InitialPage.jsx
+++ src/Pages/InitialPage/InitialPage.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import {
+    ContainerBox,
+    Box,
+    ContainerImage,
+    Image,
+    ContainerTitle,
+    Title,
+    ContainerParagraph,
+    Subtitle
+} from './InitialPage.style';
+
+import qrCode from '../../images/image-qr-code.png'
 
 export function InitialPage() {
     return (
         <>
-            <h1>Olá</h1>
-            
+            <ContainerBox>
+                <Box>
+                    <ContainerImage>
+                        <Image alt="" src={qrCode} />
+                    </ContainerImage>
+                    
+                    <ContainerTitle>
+                        <Title>
+                            Improve your front-end skills by building projects
+                        </Title>
+                    </ContainerTitle>
+
+                    <ContainerParagraph>
+                        <Subtitle>
+                            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level.
+                        </Subtitle>
+                    </ContainerParagraph>
+                </Box>
+            </ContainerBox>
         </>
     );
 }
---
+++ src/Pages/InitialPage/InitialPage.style.jsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const ContainerBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+`;
+
+export const Box = styled.div`
+    width: 300px;
+    height: 450px;
+    text-align: center;
+    border-radius: 20px;
+    background-color: hsl(0, 0%, 100%);
+    font-family: 'Outfit', sans-serif;
+`;
+
+export const ContainerImage = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+`;
+
+export const Image = styled.img`
+    width: 260px;
+    border-radius: 15px;
+`;
+
+
+export const ContainerTitle = styled.div`
+    display: inline-block;
+    max-width: 270px;
+    margin-top: 20px;
+`;
+
+export const Title = styled.div`
+    font-weight: 700;
+    font-size: 22px;
+    letter-spacing: 0.4px;
+`;
+
+
+export const ContainerParagraph = styled.div`
+    display: inline-block;
+    max-width: 260px;
+    margin-top: 10px;
+`;
+
+export const Subtitle = styled.div`
+    color: hsl(220, 15%, 55%);
+    font-weight: 400;
+    font-size: 15px;
+`;
